c1a4ef514f85642de2c1b299c4e49d2367b9e2a6
Nick Satterly <nick.satterly@guardian.co.uk>
2014-01-30 01:13:59 +0000

    add README

---
+++ README.md
@@ -0,0 +1,14 @@
+Packer.io Templates for Alerta
+==============================
+
+Used to build the vm images available for download on [alerta.io](http://alerta.io/).
+
+```
+$ packer validate alerta.json
+$ packer build -var 'aws_access_key=YOUR ACCESS KEY' -var 'aws_secret_key=YOUR SECRET KEY' alerta.json
+```
+
+License
+-------
+
+Copyright (c) 2014 Nick Satterly. Available under the MIT License.
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,49 +1,50 @@
-#!/bin/sh -x
+#!/bin/sh -e
 
-set -e
+GIT_REPO=alerta/vagrant-try-alerta
 
-echo "Waiting for network interfaces..."
-sleep 30
-
-# Update and upgrade standard packages
-sudo apt-get clean
+# Update packages
 sudo apt-get update
-#sudo apt-get upgrade
 
 # Install required dependencies
-sudo apt-get -y -q install wget git python-setuptools python-pip
-sudo apt-get -y -q install build-essential python-dev
+sudo apt-get install -y git wget python-setuptools python-pip build-essential python-dev
+sudo apt-get install -y mongodb-server rabbitmq-server apache2 libapache2-mod-wsgi
 
-# Install and Configure RabbitMQ, MongoDB, Apache web server and Java
-sudo apt-get -y -q install rabbitmq-server
+# Configure MongoDB
+grep -q smallfiles /etc/mongodb.conf || echo "smallfiles = true" | sudo tee -a /etc/mongodb.conf
+service mongodb restart
+
+# Configure RabbitMQ
 sudo /usr/lib/rabbitmq/bin/rabbitmq-plugins enable rabbitmq_management
 sudo /usr/lib/rabbitmq/bin/rabbitmq-plugins enable rabbitmq_stomp
 sudo service rabbitmq-server restart
-cd /var/tmp
-wget http://guest:guest@localhost:55672/cli/rabbitmqadmin && chmod +x rabbitmqadmin
-./rabbitmqadmin declare exchange name=alerts type=fanout
+wget -qO /var/tmp/rabbitmqadmin http://guest:guest@localhost:55672/cli/rabbitmqadmin && chmod +x /var/tmp/rabbitmqadmin
+/var/tmp/rabbitmqadmin declare exchange name=alerts type=fanout
 
-# Install and Configure MongoDB
-sudo apt-get -y -q install mongodb-server
-# create 'alerta' user
-
-# Install and Configure Apache web server
-sudo apt-get -y -q install apache2 libapache2-mod-wsgi
-
-# Install and Configure Alerta
+# Install and configure Alerta
 sudo pip install alerta
-# cd /var/tmp
-# git clone https://github.com/guardian/alerta.git
-# cd alerta
-# pip install -r requirements.txt
-# python setup.py install
+mkdir -p /etc/alerta
+cp /vagrant/files/alerta.conf /etc/alerta/alerta.conf
+cp /vagrant/files/upstart-alerta.conf /etc/init/alerta.conf
+initctl reload-configuration alerta
+service alerta restart
 
-# Install and Configure Ganglia
-sudo apt-get -y -q install ganglia-monitor gmetad
+# Configure Apache web server
+mkdir -p /var/www/alerta
+cp /vagrant/files/alerta-api.wsgi /var/www/alerta/alerta-api.wsgi
+cp /vagrant/files/httpd-alerta-api.conf /etc/apache2/conf.d/alerta-api.conf
+cp /vagrant/files/alerta-dashboard.wsgi /var/www/alerta/alerta-dashboard.wsgi
+cp /vagrant/files/httpd-alerta-dashboard.conf /etc/apache2/conf.d/alerta-dashboard.conf
+PYTHON_ROOT_DIR=`pip show alerta | awk '/Location/ { print $2 } '`
+sed -i "s#@STATIC@#$PYTHON_ROOT_DIR#" /etc/apache2/conf.d/alerta-dashboard.conf
+chmod 0775 /var/log/alerta && chgrp www-data /var/log/alerta
+service apache2 restart
 
-# Install and Configure Riemann
-sudo apt-get -y -q install default-jre
-cd /var/tmp
-wget http://aphyr.com/riemann/riemann_0.2.4_all.deb
-sudo dpkg -i /var/tmp/riemann_0.2.4_all.deb
+# Generate test alerts
+cp /vagrant/files/create-alerts.sh /var/tmp/create-alerts.sh
+chmod +x /var/tmp/create-alerts.sh && /var/tmp/create-alerts.sh
 
+pip show alerta
+
+echo "Alerta Console:  http://192.168.33.15/alerta/dashboard/v2/index.html"
+echo "Alerta API URL:  http://192.168.33.15:8080/alerta/api/v2"
+echo "Alerta Mgmt URL: http://192.168.33.15:8080/alerta/management"
