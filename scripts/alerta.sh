cb7a8325105f5654b3b14aaae151a96ddda301d4
Nick Satterly <nick.satterly@guardian.co.uk>
2014-12-17 22:36:28 +0000

    update alerta setup script

--- scripts/alerta.sh
+++ scripts/alerta.sh
@@ -48,12 +48,13 @@ from alerta.app import app as application
 EOF
 
 cat >/etc/alertad.conf << EOF
-DEBUG = True
 SECRET_KEY = '$(< /dev/urandom tr -dc A-Za-z0-9_\!\@\#\$\%\^\&\*\(\)-+= | head -c 32)'
+
 AUTH_REQUIRED = $AUTH_REQUIRED
 OAUTH2_CLIENT_ID = '$CLIENT_ID'
 ALLOWED_EMAIL_DOMAINS = ['$ALLOWED_EMAIL_DOMAIN']
-PLUGINS = ['']
+
+PLUGINS = ['reject']
 EOF
 
 echo "ServerName localhost" >> /etc/apache2/apache2.conf
