af0e414ef97716f9bbc50401a58413bee1e021fc
markayre <markayre@users.noreply.github.com>
2018-06-14 00:48:26 -0700

    Replace broken apt-get sources
    archive.ubuntu.com was migrated to old-releases.ubuntu.com so the additional command replaces it in the sources.list file as the build would break when apt-get was unable to find any packages.

--- scripts/base.sh
+++ scripts/base.sh
@@ -2,5 +2,7 @@
 
 set -x
 
+sed 's@http://us-east-1\.ec2\.archive\.ubuntu\.com/@http://old-releases.ubuntu.com/@' -i /etc/apt/sources.list
 apt-get -y update
+apt-get -y upgrade
 apt-get -y install language-pack-en git curl wget python-stdeb devscripts
